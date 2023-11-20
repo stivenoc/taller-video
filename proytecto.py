0f28ca7a67f0ac595f0e8746042151d613741c1b
stivenortiz <psrojasv@udistrital.edu.co>
2023-11-20 15:20:33 -0500

    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

--- proytecto.py
+++ proytecto.py
@@ -1,1 +1,13 @@
-"hola mundo"
+from flask import Flask, render_template
+
+class Programa:
+    def _init_(self):
+        self.app=Flask(_name_)
+        self.app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///estudiantes.sqlite3"
+        self.app.add_url_rule("/nuevo",view_func=self.agregar)
+        
+#Iniciar el servidor
+self.app.run(debug=True)
+
+def agregar(self):
+    return render_template("nuevoEstudiante.html")
